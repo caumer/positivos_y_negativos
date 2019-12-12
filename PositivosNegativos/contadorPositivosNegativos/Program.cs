4b423c03ba2b4fb660f5f683671d7a997871a752
César A. Medina R <csr.mdn@gmail.com>
2019-12-12 11:10:46 -0500

    Positivos_y_Negativos

---
+++ PositivosNegativos/contadorPositivosNegativos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace contadorPositivosNegativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el numero a evaluar");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int num = (number % 2);
+
+            if (num == 0)
+            {
+                Console.WriteLine("\n El numero ingresado es Par");
+            }
+            else
+            {
+                Console.WriteLine("\n El numero ingresado es Impar");
+            }
+
+
+
+        }
+    }
+}
